26c4a0bf32ba4cd9f58776174fe59067fa64b010
kgtrue <gammel4@gmail.com>
2023-01-06 00:04:50 +0100

    cleanup program

--- Program.cs
+++ Program.cs
@@ -3,36 +3,16 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Random_Distro;
+using Random_Distro.Runners;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) =>
-    services.AddSingleton<IRandomDistroService, RandomDistroService>())
+    .ConfigureServices((_, services) => {
+        services.AddSingleton<IRandomDistroService, RandomDistroService>();
+        services.AddSingleton<IPrintRandoDistro, PrintWithParallelFor>();
+    })
     .Build();
 
-Run(host.Services);
-host.RunAsync();
-
-static async void Run(IServiceProvider serviceProvider)
-{
-    var randomDistroService = serviceProvider.GetRequiredService<IRandomDistroService>();
-    var distroList = new ConcurrentDictionary<string, int>();
-   
-    Parallel.For(0, 100000000, new ParallelOptions() { MaxDegreeOfParallelism = 1000000 }, i =>
-    {
-        AddRandom(distroList, randomDistroService);
-    });
-
-    var max = distroList.MaxBy(kvp => kvp.Value).Key;
-
-    Console.WriteLine($"Final Distro: {max}");
-
-}
-static void AddRandom(ConcurrentDictionary<string, int> distrolist, IRandomDistroService randomDistroService)
-{
-    var distro = randomDistroService.GetDistroName();
-    distrolist.AddOrUpdate(distro, 1, (key, value) => value + 1);
-    Console.WriteLine($"Distro: {distro} distro value: {distrolist[distro]}");
-}
+host.Services.GetRequiredService<IPrintRandoDistro>().RunPrint();
