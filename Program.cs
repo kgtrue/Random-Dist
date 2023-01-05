1d7724fb8fbac907f9894c269b700744fdd8bf9a
kgtrue <gammel4@gmail.com>
2023-01-05 23:23:21 +0100

    Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Random_Distro;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    services.AddSingleton<IRandomDistroService, RandomDistroService>())
+    .Build();
+
+Run(host.Services);
+host.RunAsync();
+
+static async void Run(IServiceProvider serviceProvider)
+{
+    var randomDistroService = serviceProvider.GetRequiredService<IRandomDistroService>();
+    var distroList = new ConcurrentDictionary<string, int>();
+    var tasks = new List<Task>();
+
+    Parallel.For(0, 100000000, new ParallelOptions() { MaxDegreeOfParallelism = 1000000 }, i =>
+    {
+        AddRandom(distroList, randomDistroService);
+    });
+
+    var max = distroList.MaxBy(kvp => kvp.Value).Key;
+
+    Console.WriteLine($"Final Distro: {max}");
+
+}
+static void AddRandom(ConcurrentDictionary<string, int> distrolist, IRandomDistroService randomDistroService)
+{
+    var distro = randomDistroService.GetDistroName();
+    distrolist.AddOrUpdate(distro, 1, (key, value) => value + 1);
+    Console.WriteLine($"Distro: {distro} distro value: {distrolist[distro]}");
+}
---
+++ RandomDistroService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Distro
+{
+    public class RandomDistroService : IRandomDistroService
+    {
+        private Dictionary<int, string> distros = new Dictionary<int, string>()
+        {
+            { 1, "Arch Linux" },
+            { 2, "CentOS" },
+            { 3, "Debian GNU/Linux" },
+            { 4, "Elementary OS" },
+            { 5, "Fedora Workstation" },
+            { 6, "Kali Linux" },
+            { 7, "Manjaro Linux" },
+            { 8, "openSUSE" },
+            { 9, "Pop!_OS" },
+            { 10, "Puppy Linux" },
+            { 11, "Raspberry Pi OS" },
+            { 12, "Sabayon Linux" },
+            { 13, "Solus OS" },
+            { 14, "Ubuntu" },
+        };
+
+        public string GetDistroName()
+        {
+            Random rnd = new Random();
+            var distroNumber = rnd.Next(1, distros.Count);
+
+            return distros[distroNumber];
+        }
+    }
+}
